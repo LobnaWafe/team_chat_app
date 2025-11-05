b57d14af8acb2d2975f26ece23dfd0c89c0e95cd
LobnaWafe <lobnawafe381@gmail.com>
2025-11-05 23:06:50 +0200

    abat

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 
 void main() async {
+  // comment fi ay heta
   WidgetsFlutterBinding.ensureInitialized();
 
   // تهيئة Firebase
