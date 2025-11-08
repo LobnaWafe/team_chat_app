3540a76f107e01b2fe6f85b2c6e32c2b0c433074
LobnaWafe <lobnawafe381@gmail.com>
2025-11-08 20:39:25 +0200

    fix issue2

--- lib/features/home/presentation/views/widgets/friend_chat.dart
+++ lib/features/home/presentation/views/widgets/friend_chat.dart
@@ -3,22 +3,23 @@ import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:chats_app/features/home/data/models/chats_model.dart';
 import 'package:chats_app/features/search_users/data/models/user_model.dart';
-
 class FriendChat extends StatelessWidget {
   final ChatModel chat;
   final ChatUser friend;
   final VoidCallback? onTap;
 
-   FriendChat({
+  const FriendChat({
     super.key,
     required this.chat,
     required this.friend,
     this.onTap,
   });
 
-  final lang = CacheHelper.getData(key: "appLanguage");
   @override
   Widget build(BuildContext context) {
+    // ✅ هنا بتحملي اللغة كل مرة لما الـ widget يُبنى
+    final lang = CacheHelper.getData(key: "appLanguage") ?? "en";
+
     final time = chat.lastMessageTime != null
         ? DateFormat('hh:mm a').format(chat.lastMessageTime!)
         : '';
@@ -37,15 +38,18 @@ class FriendChat extends StatelessWidget {
           fontSize: 16,
         ),
       ),
-      subtitle:
-      chat.lastMessage==""?Align(
-        alignment:lang=="en"? Alignment.centerLeft:Alignment.centerRight,
-        child: Icon(Icons.location_on)):
-       Text(
-        chat.lastMessage,
-        maxLines: 1,
-        overflow: TextOverflow.ellipsis,
-      ),
+      subtitle: chat.lastMessage == ""
+          ? Align(
+              alignment: lang == "en"
+                  ? Alignment.centerLeft
+                  : Alignment.centerRight,
+              child: const Icon(Icons.location_on),
+            )
+          : Text(
+              chat.lastMessage,
+              maxLines: 1,
+              overflow: TextOverflow.ellipsis,
+            ),
       trailing: Text(
         time,
         style: const TextStyle(fontSize: 12, color: Colors.grey),
